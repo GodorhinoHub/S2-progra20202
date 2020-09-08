668da7be91e0ecc76a8cd2fb4ebc4e6dce98ccf1
GodorhinoHub <70235282+GodorhinoHub@users.noreply.github.com>
2020-09-08 17:41:16 -0300

    Add files via upload

---
+++ EjercicioPersona/Cliente.java
@@ -0,0 +1,11 @@
+public class Cliente extends Persona{
+    String address;
+
+    public void setAddress(String address){
+
+    }
+
+    public String getAddress(){
+        return this.address;
+    }
+}
---
+++ EjercicioPersona/Persona.java
@@ -0,0 +1,15 @@
+public class Persona {
+    String name;
+
+    Persona(){
+
+    }
+
+    Persona(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+}
