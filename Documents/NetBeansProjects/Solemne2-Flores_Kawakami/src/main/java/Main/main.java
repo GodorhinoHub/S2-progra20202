3c8d389bef2a2501ec0f665c488d9c9ae117cf02
viktoruz66 <viktoruz66@gmail.com>
2020-11-11 17:22:14 -0300

    Inicio del uso de GitHub

--- Documents/NetBeansProjects/Solemne2-Flores_Kawakami/src/main/java/Main/main.java
+++ Documents/NetBeansProjects/Solemne2-Flores_Kawakami/src/main/java/Main/main.java
@@ -5,13 +5,13 @@ import java.sql.*;
 public class main {
     public static void main(String[] args) throws ClassNotFoundException, SQLException{
 	Connection conn = DriverManager.getConnection(
-		"jdbc:mysql://localhost:3306/instituto","standart","1234standart");
+		"jdbc:mysql://localhost:3306/colegio","standart","1234standart");
 	Statement stmt = conn.createStatement();
 	ResultSet rs = stmt.executeQuery(
- 		"SELECT apellido FROM Usuarios");
+ 		"SELECT login FROM Administrador");
 		
 	while (rs.next()) {
-		String name = rs.getString("apellido");
+		String name = rs.getString("login");
 		System.out.println(name);
 	}
 	rs.close();
